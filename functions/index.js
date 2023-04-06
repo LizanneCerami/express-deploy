3b4ccfc026d69d282e121d8164ad16cc81e5c907
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-04-06 16:17:44 -0400

    Added code into index.js

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,23 @@
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
-import { addEmployee, getAllEmployees } from "./src/employees.js";
+import { addEmployee, deleteEmployee, getAllEmployees, updateEmployee } from "./src/employees.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json()); // needed for POST and PATCH
 
-app.get('/employees', getAllEmployees);
+app.get("/", (req, res) => {
+  res.send("I am gRoot.");
+});
 
 app.get('/test', (req, res) => {
   res.send("My cloud function API is working! 🤯");
 });
 
-app.post("/employees", addEmployee)
+app.post('/employees', addEmployee);
+app.get('/employees', getAllEmployees);
+app.delete('/employees/:id', deleteEmployee);
+app.patch('/employees/:id', updateEmployee);
 
 export const api = functions.https.onRequest(app);

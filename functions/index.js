fde016fb727f30be2cce9f4cd772e7adbddebfe1
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-04-06 16:07:37 -0400

    Updated and deleted employees

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,18 @@
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
+import { addEmployee, getAllEmployees } from "./src/employees.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json()); // needed for POST and PATCH
 
+app.get('/employees', getAllEmployees);
+
 app.get('/test', (req, res) => {
   res.send("My cloud function API is working! 🤯");
 });
 
-app.get('/hello', (req, res) => {
-  res.send("Hello there!!")
-});
+app.post("/employees", addEmployee)
 
 export const api = functions.https.onRequest(app);
